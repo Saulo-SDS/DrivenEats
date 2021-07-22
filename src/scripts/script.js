ad712a7e1e78ae1205fce83e2ea1173e264d5443
Saulo <saulosilva@super.ufam.edu.br>
2021-07-22 12:40:48 -0400

    Botão de finalizar pedido adicionado

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -2,6 +2,19 @@ let comida_selecionada = 0;
 let bebida_selecionada = 0;
 let sobremesa_selecionada = 0;
 
+function realizarPedido(){
+    if(comida_selecionada > 0 && bebida_selecionada > 0 && sobremesa_selecionada > 0){
+        const elemento = document.querySelector("button");
+        elemento.removeAttribute("disabled");
+        elemento.classList.add("botao-pedido-confirmacao");
+        elemento.innerHTML = 'Fechar pedido';
+    }
+}
+
+function teste(){
+    alert("funcionando perfeitamente");
+}
+
 function selecionaComida(opcao) {
     if (comida_selecionada != 0) resetaOpcaoComida(opcao);
 
@@ -11,28 +24,29 @@ function selecionaComida(opcao) {
     const icone = document.querySelectorAll("ion-icon");
     icone[opcao-1].style.color = "#32B72F";
     comida_selecionada = opcao;
+    realizarPedido();
 }
 
 function selecionaBebida(opcao) {
     if (bebida_selecionada != 0) resetaOpcaoBebida(opcao);
-
     let nome = ".bebida" + opcao;
     const elemento = document.querySelector(nome);
     elemento.classList.add("opcao-selecionada-bebida");
     const icone = document.querySelectorAll(".bebida");
     icone[opcao-1].style.color = "#32B72F";
     bebida_selecionada = opcao;
+    realizarPedido();
 }
 
 function selecionaSobremesa(opcao) {
     if (sobremesa_selecionada != 0) resetaOpcaoSobremesa(opcao);
-
     let nome = ".sobremesa" + opcao;
     const elemento = document.querySelector(nome);
     elemento.classList.add("opcao-selecionada-sobremesa");
     const icone = document.querySelectorAll(".sobremesa");
     icone[opcao-1].style.color = "#32B72F";
     sobremesa_selecionada = opcao;
+    realizarPedido();
 }
 
 
