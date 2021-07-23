6c9cda4d9d4deb0e4aaa4e34c9c30bc801e7162d
Saulo <saulosilva@super.ufam.edu.br>
2021-07-22 21:36:37 -0400

    Adicionado Envio do pedido a loja

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -11,8 +11,9 @@ function realizarPedido(){
     }
 }
 
-function teste(){
-    alert("funcionando perfeitamente");
+function realizarPedido(){
+    alert("funcionando...");
+    enviaPedido();
 }
 
 function selecionaComida(opcao) {
@@ -29,6 +30,7 @@ function selecionaComida(opcao) {
 
 function selecionaBebida(opcao) {
     if (bebida_selecionada != 0) resetaOpcaoBebida(opcao);
+
     let nome = ".bebida" + opcao;
     const elemento = document.querySelector(nome);
     elemento.classList.add("opcao-selecionada-bebida");
@@ -40,6 +42,7 @@ function selecionaBebida(opcao) {
 
 function selecionaSobremesa(opcao) {
     if (sobremesa_selecionada != 0) resetaOpcaoSobremesa(opcao);
+
     let nome = ".sobremesa" + opcao;
     const elemento = document.querySelector(nome);
     elemento.classList.add("opcao-selecionada-sobremesa");
@@ -69,4 +72,49 @@ function resetaOpcaoSobremesa(opcao){
     const anterior_checado = document.querySelectorAll(".sobremesa");
     anterior.classList.remove("opcao-selecionada-sobremesa");
     anterior_checado[sobremesa_selecionada-1].style.color = "white";
+}
+
+
+
+function enviaPedido(){
+
+    const element_comida = document.querySelector(".nome-comida" + comida_selecionada);
+    const element_bebida = document.querySelector(".nome-bebida" + bebida_selecionada);
+    const element_sobremesa = document.querySelector(".nome-sobremesa" + sobremesa_selecionada);
+
+    const element_preco_comida = document.querySelector(".preco-comida" + comida_selecionada);
+    const element_preco_bebida = document.querySelector(".preco-bebida" + bebida_selecionada);
+    const element_preco_sobremesa = document.querySelector(".preco-sobremesa" + sobremesa_selecionada);
+   
+    let nome_comida = element_comida.innerHTML
+    let nome_bebida = element_bebida.innerHTML
+    let nome_sobremesa = element_sobremesa.innerHTML
+
+    let valor_comida = element_preco_comida.innerHTML;
+    let valor_bebida = element_preco_bebida.innerHTML;
+    let valor_sobremesa = element_preco_sobremesa.innerHTML;
+
+    let valor_total = ValorPedido(valor_comida) + ValorPedido(valor_bebida) + ValorPedido(valor_sobremesa);
+
+    let pedido = `Olá, gostaria de fazer o pedido:\n` + 
+                 `- Prato: ${nome_comida}\n` + 
+                 `- Bebida: ${nome_bebida}\n` + 
+                 `- Sobremesa: ${nome_sobremesa}\n` +
+                 `Total: ${valor_total.toFixed(2)}\n`;
+
+    /*
+    console.log(nome_comida);
+    console.log(nome_bebida);
+    console.log(nome_sobremesa);
+    console.log(valor_total.toFixed(2));
+    console.log(pedido);*/
+
+    const url = "https://wa.me/5592994939981?text=" + encodeURIComponent(pedido);
+    window.open(url);
+}
+
+function ValorPedido(valor){
+    valor = valor.replace('R$ ','');
+    valor = valor.replace(',','.');
+    return Number(valor);
 }
